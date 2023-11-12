8fe7e5c8105457a0d2bcb347d4c96b613b1fefd2
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-12 22:08:00 +0530

    added new changes

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,15 +2,6 @@
 import { Fragment, useEffect, useRef } from 'react'
 import transition from '../Transition'
 import style from './Skills.module.css'
-import htmllogo from '../Images/png-transparent-html5-icon-•-html-social-network-icon 1.png'
-import csslogo from '../Images/png-transparent-html5-icon-•-css-social-network-icon 1.png'
-import jslogo from '../Images/png-transparent-html5-icon-•-js-social-network-icon 1.png'
-import reactjslogo from '../Images/png-transparent-html5-icon-•-react-social-network-icon 1.png'
-import bootstraplogo from '../Images/png-transparent-html5-icon-•-boot-social-network-icon 1.png'
-import figmalogo from '../Images/png-transparent-html5-icon-•-figma-social-network-icon 1.png'
-import muilogo from '../Images/png-transparent-html5-icon-•-mui-social-network-icon 1.png'
-import gitlogo from '../Images/png-transparent-html5-icon-•-git-social-network-icon 1.png'
-import gitlablogo from '../Images/png-transparent-html5-icon-•-gitlab-social-network-icon 1.png'
 import gsap from 'gsap'
 
 function Skills() {
@@ -38,57 +29,53 @@ function Skills() {
         });
     }, []);
 
+
+    const skills = [
+
+        {
+            title: "Html",
+        },
+
+        {
+            title: "Css",
+        },
+
+        {
+            title: "JavaScript",
+        },
+
+        {
+            title: "React Js",
+        },
+
+        {
+            title: "Material UI",
+        },
+
+        {
+            title: "Bootstrap",
+        },
+
+        {
+            title: "Figma",
+        },
+    ]
+
     return (
         <Fragment>
             <div className={style.main_skills_add}>
                 <h1 ref={h1Ref}>Skills</h1>
-                <section className={style.services_container}>
-                    <div className={style.row}>
-                        <div className={style.item}>
-                            <img src={htmllogo} alt="" />
-                            <h2>HTML</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={csslogo} alt="" />
-                            <h2>CSS</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={jslogo} alt="" />
-                            <h2>Javascript</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={reactjslogo} alt="" />
-                            <h2>React Js</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={bootstraplogo} alt="" />
-                            <h2>Bootstrap</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={figmalogo} alt="" />
-                            <h2>Figma</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={muilogo} alt="" />
-                            <h2>Material UI</h2>
-                        </div>
-
-                        <div className={style.item}>
-                            <img src={gitlogo} alt="" />
-                            <h2>Github</h2>
-                        </div>
-                        <div className={style.item}>
-                            <img src={gitlablogo} alt="" />
-                            <h2>Git</h2>
-                        </div>
+                <div className={style.p_main_skills}>
+                    <div className={style.sub_main_skills}>
+                        {
+                            skills.map((items, indexes) => (
+                                <div className={style.primary_skills_imp} key={indexes}>
+                                    <p>{items.title}</p>
+                                </div>
+                            ))
+                        }
                     </div>
-                </section>
+                </div>
             </div>
         </Fragment>
     )
