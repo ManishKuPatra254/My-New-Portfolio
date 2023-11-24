a70a638dedd7dcb8a0acb91019c6195550d9a25c
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-24 12:04:49 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='*' element={<App />} />
+        <Route path='/*' element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
